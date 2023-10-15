9a585f6b26047234e5a15f8a5e0441d8fafc7e2e
NRYNSH02 <nryn4cp@gmail.com>
2023-10-16 01:24:56 +0530

    ADDED SUBTRACT FUNCTION

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,10 @@
 def addition(a,b):
     return a+b
 
+def subtract(a,b):
+    return abs(a-b)
 x = int(input("Enter 1st number: "))
 y = int(input("Enter 2nd number: "))
 
 print("x+y = ", addition(x,y))
+print("x+y = ", subtract(x,y))
